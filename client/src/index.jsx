c7aba5d7b774e8cceedf81bd1cbad813cd4a8402
Kelly <kellytso04@gmail.com>
2021-12-03 16:04:18 -0500

    Adds styled container

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
+import styled from 'styled-components';
 import NamePrompt from './welcome/NamePrompt.jsx';
 import Dashboard from './welcome/Dashboard.jsx';
+
+const AppContainer = styled.div`
+  border: #f7d088 5px solid;
+  border-radius: 10px;
+  padding: 10px;
+`
 
 const App = () => {
   const [ name, setName ] = useState('');
@@ -10,7 +17,11 @@ const App = () => {
 
   return (
     <div className='app'>
+      <AppContainer >
       { name.length ? <Dashboard name={name} /> : <NamePrompt setName={setName} /> }
+
+
+      </AppContainer>
     </div>
   )
 }
