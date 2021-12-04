08dc1b188e9aac365c96077534e8b81b86f26487
Kelly <kellytso04@gmail.com>
2021-12-04 12:10:59 -0500

    Adds 'Done' button that sets reminder as completed'

--- client/src/reminders/Reminder.jsx
+++ client/src/reminders/Reminder.jsx
@@ -2,16 +2,20 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 // {reminder, day, time, location, priority, details, hex_color}
-const Reminder = ( {text, no} ) => {
+const Reminder = ( {text, handleComplete, no} ) => {
   const reminderID = `reminder-text-${no}`;
+
   const handleClick = () => {
     const reminderText = document.getElementsByClassName(reminderID)[0];
     reminderText.style.textDecorationLine = 'line-through';
+
+    handleComplete(text);
   }
 
   return (
     <div className='reminder'>
-      <span className={reminderID} onClick={handleClick}>{text}</span>
+      <span className={reminderID}>{text}</span>
+      <button onClick={handleClick}>Done</button>
     </div >
   )
 }
