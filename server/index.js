7558f67e7672b81448d101e997a555be049abbb5
Kelly <kellytso04@gmail.com>
2021-12-03 21:09:03 -0500

    Builds out all routes

--- server/index.js
+++ server/index.js
@@ -1,78 +1,162 @@
 const express = require('express');
 const app = express();
+
+const { client } = require('../db/config.js');
 const habit = require('./models/habitModel.js');
 const task = require('./models/taskModel.js');
 const reminder = require('./models/reminderModel.js');
 
 // -- Middleware ----------------------------------------------------------------------
-app.use(express.static(__dirname + '/client/dist'));
+app.use(express.static(__dirname + '/../client/dist'));
 app.use(express.json());
 app.use(express.urlencoded( {extended: true} ));
 
 // -- Routes --------------------------------------------------------------------------
 app.get('/habits', (req, res) => {
-  console.log('GET DIS BREAD');
-  habit.fetch()
-    .then( (habits) => {
-      console.log(habits);
-      res.status(200).send(habits);
-    })
-    .catch( (err) => {
+  const queryString = `SELECT * FROM habits`;
+
+  client.query(queryString, (err, habits) => {
+    if (err) {
       console.error(err);
-      console.error('Error fetching habits');
-    })
+    }
+      console.log('Received back: ', habits);
+      res.status(200).send(habits);
+  });
 });
 
 app.post('/habits', (req, res) => {
-  console.log('REQ.BODY: ', req.body);
-  habit.create(req.body)
-    .then( () => {
-      res.send(201);
-    })
-    .catch( (err) => {
+  const queryString = `INSERT INTO habits (habit, date, hex_color) VALUES(?, ?, ?)`;
+  const queryArgs = [req.body.habit, req.body.date, req.body.hex_color];
+
+  return client.query(queryString, queryArgs, (err) => {
+    if (err) {
       console.error(err);
-      console.error('Error creating new habit');
-    })
+    } else {
+      res.sendStatus(201);
+    }
+  });
 });
 
 app.put('/habits', (req, res) => {
+  const queryString = `UPDATE habits SET habit = ${req.body.updateText} WHERE habit = ${req.body.habit}`;
 
+  return client.query(queryString, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.sendStatus(202);
+    }
+  });
 });
 
 app.delete('/habits', (req, res) => {
+  const queryString = `DELETE FROM habits WHERE habit = ${req.body.data.habit}`;
 
+  return client.query(queryString, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.sendStatus(204);
+    }
+  });
 });
 
 app.get('/tasks', (req, res) => {
-  res.send(200);
+  const queryString = `SELECT * FROM tasks WHERE completed = 0`;
+
+  client.query(queryString, (err, tasks) => {
+    if (err) {
+      console.error(err);
+    }
+      console.log('Received back: ', tasks);
+      res.status(200).send(tasks);
+  });
 });
 
 app.post('/tasks', (req, res) => {
+  const queryString = `INSERT INTO tasks (task, category, hex_color) VALUES(?, ?, ?)`;
+  const queryArgs = [req.body.habit, req.body.category, req.body.hex_color];
 
+  return client.query(queryString, queryArgs, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.sendStatus(201);
+    }
+  });
 });
 
 app.put('/tasks', (req, res) => {
+  const queryString = `UPDATE tasks SET task = ${req.body.updateText} WHERE task = ${req.body.task}`;
 
+  return client.query(queryString, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.sendStatus(202);
+    }
+  });
 });
 
 app.delete('/tasks', (req, res) => {
+  const queryString = `DELETE FROM tasks WHERE task = ${req.body.data.task}`;
 
+  return client.query(queryString, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.sendStatus(204);
+    }
+  });
 });
 
 app.get('/reminders', (req, res) => {
+  const queryString = `SELECT * FROM reminders`;
 
+  client.query(queryString, (err, reminders) => {
+    if (err) {
+      console.error(err);
+    }
+      console.log('Received back: ', reminders);
+      res.status(200).send(reminders);
+  });
 });
 
 app.post('/reminders', (req, res) => {
+  const queryString = `INSERT INTO reminders (reminder, day, time, location, priority, details, hex_color) VALUES(?, ?, ?, ?, ?, ?, ?)`;
+  const queryArgs = [req.body.reminder, req.body.day, req.body.time, req.body.location, req.body.priority, req.body.details, req.body.hex_color];
 
+  return client.query(queryString, queryArgs, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.sendStatus(201);
+    }
+  });
 });
 
 app.put('/reminders', (req, res) => {
+  const queryString = `UPDATE reminders SET reminder = ${req.body.updateText} WHERE reminder = ${req.body.reminder}`;
 
+  return client.query(queryString, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.sendStatus(202);
+    }
+  });
 });
 
 app.delete('/reminders', (req, res) => {
+  const queryString = `DELETE FROM reminders WHERE reminder = ${req.body.data.reminder}`;
 
+  return client.query(queryString, (err) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.sendStatus(204);
+    }
+  });
 });
 
 // ------------------------------------------------------------------------------------
