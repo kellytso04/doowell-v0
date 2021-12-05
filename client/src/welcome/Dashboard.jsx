d70482ba98c0df180e0149d40415e30bdedf4499
Kelly <kellytso04@gmail.com>
2021-12-05 11:45:39 -0500

    Renders # of inc tasks and reminders

--- client/src/welcome/Dashboard.jsx
+++ client/src/welcome/Dashboard.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { DashboardContainer } from '../../styles/dashboard.styled.js';
+import { fetchIncompleteTasks, fetchIncompleteReminders } from '../helper.js';
 import Clock from './Clock.jsx';
 
 const Dashboard = ( {name} ) => {
+  const [ numIncTasks, setNumIncTasks ] = useState([]);
+  const [ numIncReminders, setNumIncReminders ] = useState([]);
+
+  useEffect( () => {
+    fetchIncompleteTasks()
+      .then( ({data}) => {
+        setNumIncTasks(data.length);
+
+        fetchIncompleteReminders()
+          .then( ({data}) => {
+            setNumIncReminders(data.length);
+          })
+          .catch( (err) => {
+            console.error(err);
+          })
+      })
+      .catch( (err) => {
+        console.error(err);
+      });
+  }, []);
+
   return (
     <DashboardContainer className='dashboard'>
       <div className='welcome-msg'>Welcome back, {name}</div>
       <hr />
       <Clock />
-      {/* <br />
       <br />
-      You have # unfinished tasks and # reminders. */}
+      <br />
+      You have {numIncTasks} unfinished tasks and {numIncReminders} reminders.
     </DashboardContainer>
   )
 }
