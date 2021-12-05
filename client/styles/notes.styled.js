253eaa7eecc21dda1654dcc650f1eb345ffbf119
Kelly <kellytso04@gmail.com>
2021-12-05 17:20:51 -0500

    Creates style file for Note component

---
+++ client/styles/notes.styled.js
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const StyledNotes = styled.form`
+  display: flex;
+  flex-direction: column;
+  background-color: #A8D0E6;
+  font-family: 'Varela Round', sans-serif;
+  color: white;
+  border: #24305E 5px solid;
+`
+
+const StyledTextArea = styled.textarea`
+  height: 250px;
+  width: 700px;
+  font-family: 'EB Garamond', serif;
+  color: #F76C6C;
+`
