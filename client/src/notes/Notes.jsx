7c63a7a886ece958782573ce1a98a1bf56372feb
Kelly <kellytso04@gmail.com>
2021-12-06 11:10:02 -0500

    Notes are saved on an interval of 2s instead of having the user manually save the notes

--- client/src/notes/Notes.jsx
+++ client/src/notes/Notes.jsx
@@ -3,49 +3,80 @@ import { StyledNotes, StyledTextArea } from '../../styles/notes.styled.js';
 import { fetchNotes, postNotes, editNotes } from '../helper.js';
 
 const Notes = (props) => {
+  const [ previousNotes, setPreviousNotes ] = useState('');
   const [ notes, setNotes ] = useState('');
 
   useEffect( () => {
     fetchNotes()
       .then( ({data}) => {
         setNotes(data.notes);
+        setPreviousNotes(data.notes);
       })
       .catch( (err) => {
         console.error(err);
       });
   }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
+  useEffect( () => {
+    const intervalID = setInterval( () => {
+      // Every 3s, we check if the user has added notes
+      // If they have, we'll save the notes to the database
 
-    editNotes(notes)
-      .then( () => {
-        fetchNotes()
-          .then( ({data}) => {
-            setNotes(data.notes);
+      if (notes !== previousNotes) {
+        console.log('saving new notes');
+        editNotes(notes)
+          .then( () => {
+            fetchNotes()
+            .then( ({data}) => {
+              setNotes(data.notes);
+              setPreviousNotes(data.notes);
+            })
+            .catch( (err) => {
+              console.error(err);
+            });
           })
           .catch( (err) => {
             console.error(err);
-          });
-      })
-      .catch( (err) => {
-        console.error(err);
-      });
-  }
+          })
+      }
+    }, 2000);
+
+    return function cleanup() {
+      clearInterval(intervalID);
+    }
+  }, [notes]);
 
   return (
-    <div className='notes-container' style={{fontFamily: 'Varela Round, sans-serif'}}>
+    <div className='notes-container' style={{fontFamily: 'Varela Round, sans-serif', marginTop: '20px'}}>
       <div className='notes-msg'> 💡 Notes</div>
-      <StyledNotes onSubmit={handleSubmit}>
+      <StyledNotes >
         <StyledTextArea
           className='notes'
           value={notes}
           placeholder={'Jot important stuff down here...'}
           onChange={(e) => {setNotes(e.target.value)}} />
-        <input type='submit' value='Save' style={{backgroundColor: 'white', border: '#F76C6C 2px solid'}}/>
       </StyledNotes >
     </div>
   )
 }
 
 export default Notes;
+
+// const handleSubmit = (e) => {
+//   e.preventDefault();
+
+//   editNotes(notes)
+//     .then( () => {
+//       fetchNotes()
+//         .then( ({data}) => {
+//           setNotes(data.notes);
+//         })
+//         .catch( (err) => {
+//           console.error(err);
+//         });
+//     })
+//     .catch( (err) => {
+//       console.error(err);
+//     });
+// }
+// <input type='submit' value='Save' style={{backgroundColor: 'white', border: '#F76C6C 2px solid'}}/>
